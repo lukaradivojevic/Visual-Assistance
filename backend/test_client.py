7a34aff5c7b0fc0bc6a2abb623222f76e06fcf99
Dusan Jovanovic <dule.jo.2005@gmail.com>
2026-05-06 18:14:46 +0200

    Small change in test_client.py

--- backend/test_client.py
+++ backend/test_client.py
@@ -18,7 +18,7 @@ def image_to_base64(image_path: str) -> str:
     with open(image_path, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
 
-    return "data:image/jpeg;base64," + encoded_image
+    return encoded_image
 
 
 def send_request(image_base64: str, mode: str):
